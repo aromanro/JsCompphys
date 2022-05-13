c4afe264a9192fb89013a4e019078e1f5850bdd4
Adrian Roman <a.roman@ib-company.de>
2022-05-13 11:16:46 +0300

    Added files for Monte Carlo

---
+++ RandomWalk.js
@@ -0,0 +1,36 @@
+(function() {
+ var canvas = document.getElementById("randomWalkCanvas");
+ var ctx = canvas.getContext("2d");
+ ctx.strokeStyle = "#000088";
+
+ var dist = canvas.height / 2.;
+ ctx.translate(dist, dist);
+
+ var posX = 0;
+ var posY = 0;
+
+ function randomWalk() {
+    var dir = Math.floor(Math.random()*2);
+    var sense = Math.floor(Math.random()*2);
+
+    ctx.beginPath();
+    ctx.moveTo(posX,posY);
+
+    if (dir == 0) 
+        posX += (sense ? 1 : -1)*4;
+    else
+        posY += (sense ? 1 : -1)*4;     
+
+    ctx.lineTo(posX,posY);
+    ctx.stroke();
+
+    if (Math.abs(posX) > dist || Math.abs(posY) > dist)
+    {
+      posX = 0;
+      posY = 0;
+      ctx.clearRect(-dist, -dist, canvas.width, canvas.height);
+    }
+ }
+
+ setInterval(randomWalk, 50);
+})();
---
+++ pi.js
@@ -0,0 +1,41 @@
+(function() {
+ var piText = document.getElementById("piText");
+ var canvas = document.getElementById("piCanvas");
+ var ctx = canvas.getContext("2d");
+
+ var radius = canvas.height / 2.;
+ ctx.translate(radius, radius);
+
+ var totalPoints = 0;
+ var insidePoints = 0;
+
+ function randomPoint() {
+    var x = 2.*Math.random()-1.; 
+    var y = 2.*Math.random()-1.;
+
+    var p = x*x + y*y;
+    if (p < 1) {
+        ctx.fillStyle = "#FF0000";
+        ++insidePoints;
+    }
+    else ctx.fillStyle = "#444444";
+  
+    ++totalPoints;
+    ctx.fillRect(radius*x-1,radius*y-1,2,2);
+
+    if (totalPoints % 100 === 0)
+    {
+       var piApprox = insidePoints / totalPoints * 4.;
+       piText.innerHTML = piApprox.toPrecision(3);
+       if (piText.innerHTML == "3.14")
+       {
+           totalPoints = 0;
+           insidePoints = 0;
+
+           ctx.clearRect(-radius, -radius, canvas.width, canvas.height);
+       }
+    }
+ }
+
+ setInterval(randomPoint, 10);
+})();
